dc97164ae87c10e3faf418ae70ab6a1dc3b73378
bolahuyy19-cyber <bolahuyy19@gmail.com>
2026-03-01 14:42:13 +0700

    Add files via upload

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "sun",
+  "version": "1.0.0",
+  "main": "sun.js",
+  "type": "commonjs",
+  "scripts": {
+    "sun": "node sun.js",
+    "start": "node sun.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "axios": "^1.6.8"
+  }
+}
